735b27f1ae8337891afc109517b8d2e58538750c
Nhat nguyen <nhathk2410@gmail.com>
2022-10-23 20:51:13 +0700

    add data

---
+++ .history/src/Components/Layout/Components/Header/index_20221013215416.js
@@ -0,0 +1,15 @@
+import classNames from 'classnames/bind';
+import React from 'react';
+import styles from '../../Components/Header/Header.module.scss';
+
+const cx = classNames.bind(styles);
+
+export default function Header() {
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}></div>
+            </div>
+        </header>
+    );
+}
